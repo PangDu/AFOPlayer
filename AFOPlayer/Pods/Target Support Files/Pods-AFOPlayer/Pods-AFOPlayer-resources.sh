6a791bac8726eabfdb1451fecb39792f8c827435
PangDu <792229197@qq.com>
2026-04-07 17:00:15 +0800

    update

--- AFOPlayer/Pods/Target Support Files/Pods-AFOPlayer/Pods-AFOPlayer-resources.sh
+++ AFOPlayer/Pods/Target Support Files/Pods-AFOPlayer/Pods-AFOPlayer-resources.sh
@@ -97,9 +97,11 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "${PODS_ROOT}/../../../AFOFFMpeg/AFOFFMpeg/play/AFOMetalShaders.metal"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/FMDB/FMDB_Privacy.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "${PODS_ROOT}/../../../AFOFFMpeg/AFOFFMpeg/play/AFOMetalShaders.metal"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/FMDB/FMDB_Privacy.bundle"
 fi
 
